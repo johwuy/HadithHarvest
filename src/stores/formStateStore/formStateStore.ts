7e084b2ae3d6efd9f7c0d32ee130e53cf53bde3e
johwuy <johnnyw8@uci.edu>
2024-09-13 22:10:33 -0700

    create form store

---
+++ src/stores/formStateStore/formStateStore.ts
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import { HadithKey } from "../../lib/hadithType";
+
+const INITIAL_SOURCES: HadithKey[] = [
+    "bukhari",
+    "muslim",
+    "abudawud",
+    "tirmidhi",
+    "nasai",
+    "ibnmajah",
+];
+
+export const STARTING_COUNT = 1;
+
+type FormState = {
+    isLoading: boolean;
+    hadithSources: HadithKey[];
+    hadithCount: number;
+    setHadithSources: (newSources: HadithKey[]) => void;
+    removeHadithSource: (source: HadithKey) => void;
+    setHadithCount: (newCount: number) => void;
+    toggleLoading: () => void;
+};
+
+export const useFormStateStore = create<FormState>((set) => ({
+    isLoading: false,
+    hadithSources: INITIAL_SOURCES,
+    hadithCount: STARTING_COUNT,
+    toggleLoading: () => set((prev) => ({ isLoading: !prev.isLoading })),
+    setHadithSources: (newSources: HadithKey[]) =>
+        set(() => ({ hadithSources: newSources })),
+    removeHadithSource: (toRemoveSource: HadithKey) =>
+        set((state) =>
+            state.hadithSources.length > 1
+                ? {
+                      hadithSources: state.hadithSources.filter(
+                          (hadith) => hadith != toRemoveSource,
+                      ),
+                  }
+                : { hadithSources: state.hadithSources },
+        ),
+    setHadithCount: (newCount: number) =>
+        set(() => ({ hadithCount: newCount >= 1 ? Math.round(newCount) : 1 })),
+}));
